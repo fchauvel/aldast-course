54942a14b9aad8715c05a37c220aa3ba34e873b3
Franck Chauvel <franck.chauvel@gmail.com>
2021-03-30 12:30:57 +0200

    chore: Built the reveal.js-theme using Gulp

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const connect = require('gulp-connect');
+
+gulp.task('install-reveal.js', () =>  {
+    const source = 'src/assets/lib/reveal.js';
+    const target = 'site/lib/reveal.js';
+    gulp.src([`${source}/dist/**/*`])
+        .pipe(gulp.dest(`${target}/dist`));
+    return gulp.src([`${source}/plugin/**/*`])
+        .pipe(gulp.dest(`${target}/plugin`));
+});
+
+gulp.task('install-reveal-plugins', () => {
+ return gulp
+        .src(['src/assets/lib/reveal.js/plugin/**/*'])
+        .pipe(gulp.dest('site/lib/reveal.js/plugin'));
+
+});
+
+gulp.task('install-reveal-theme', () => {
+    const source = 'src/assets/lib/reveal.js-theme';
+    const target = 'site/lib/reveal.js-theme/';
+    gulp.src(`${source}/scss/nord-reveal.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(target));
+     gulp.src(`${source}/scss/nord.css`)
+        .pipe(gulp.dest(target));
+    return gulp.src(`${source}/js/nord-reveal.js`)
+        .pipe(gulp.dest(target));
+});
+
+gulp.task('install-site', () =>  {
+    return gulp
+        .src(['src/**/*.html', '!src/assets/**/*.html'])
+        .pipe(gulp.dest('site/'));
+
+});
+
+gulp.task('build',
+          gulp.parallel([
+              'install-reveal.js',
+              'install-reveal-plugins',
+              'install-reveal-theme',
+              'install-site']));
+
+gulp.task('clean', () => {
+    return del([
+        'site',
+    ]);
+});
+
+
+gulp.task('serve', () => {
+    const port = 8000;
+    const root = "./site";
+    connect.server({
+        root: root,
+        port: port,
+        host: '0.0.0.0',
+        livereload: true
+    });
+});
+
+
+gulp.task('default',
+          gulp.series(['build']));
