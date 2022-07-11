7e289f3e0bd265fb76dc1e4970ebee91850ce5ad
Franck Chauvel <franck.chauvel@gmail.com>
2022-07-11 07:03:28 +0200

    clean-up: New Lecture organizations

--- lectures/2.1_efficiency/notes/code/sum.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int sum = 0;
-  for (int any=0; any<=100; any++) {
-    sum += any;
-  }
-  printf("Sum: %d\n", sum);
-}
--- lectures/2.1_efficiency/notes/code/sum.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-sum = 100 * 101 / 2
-print("Sum: ", sum)
